03badac463f111a6da4ceddbfe9706a63b39f552
unknown <redxverify@gmail.com>
2025-03-23 03:49:17 -0400

    ðŸ„ Auto-update: bumped to

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RedXAIEasyFunctions",
-    version="0.1.0",
+    version="1.0.2",
     description="A Python toolkit to simplify and enhance basic functions.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
